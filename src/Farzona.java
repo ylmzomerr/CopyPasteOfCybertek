de302d659614850369c772f46d08267ee27c1a10
ylmzomerr <56378540+ylmzomerr@users.noreply.github.com>
2019-11-18 06:22:11 -0500

    ready

--- src/Farzona.java
+++ src/Farzona.java
@@ -1,13 +1,4 @@
 public class Farzona {
 
-    public static int multiplication(int num1, int num2){
 
-        int multi = num1 * num2;
-        return multi;
-    }
-    public  static  int division(int num1, int num2){
-
-        int divis = num1 / num2;
-        return  divis;
-    }
 }

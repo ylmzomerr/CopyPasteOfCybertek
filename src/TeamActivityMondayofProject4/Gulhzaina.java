a795000cfad32d91eea3bc872383bc27a8d52b3f
Gulzhaina <nazhmadinovagn@gmail.com>
2019-11-18 21:33:57 -0600

    adding

--- src/TeamActivityMondayofProject4/Gulhzaina.java
+++ src/TeamActivityMondayofProject4/Gulhzaina.java
@@ -1,4 +1,28 @@
 package TeamActivityMondayofProject4;
 
 public class Gulhzaina {
+    public static boolean isAnagram(String word1, String word2) {
+
+        word1 = word1.toLowerCase().trim();
+        word2 = word2.toLowerCase().trim();
+        int count=0;
+        if(word1.contains(" ")){
+            word1=word1.replace(" ","");
+        }
+        if(word2.contains(" ")){
+            word2=word2.replace(" ","");
+        }
+
+        if(word1.length()==word2.length()){
+            for (int i = 0; i < word1.length(); i++) {
+                if (word2.contains(word1.charAt(i) + "")) {
+                    count++;
+                }
+            }
+            if (count == word1.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

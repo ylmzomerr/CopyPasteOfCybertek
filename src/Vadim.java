69a9c8782fc0b6c38f96191aaa81a284ff01d660
Vadim Bacalov <bacalov.vadim@gmail.com>
2019-11-18 18:56:08 -0600

    new

--- src/Vadim.java
+++ src/Vadim.java
@@ -1,2 +1,10 @@
 public class Vadim {
+    public int sumOfTwo(int a, int b){
+
+        // adding two integer value.
+        int sum = a + b;
+
+        //returning summation of two values.
+        return sum;
+    }
 }

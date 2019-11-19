f132ba91d4aa88d1f553494d9df4b1ca78a56bd4
mariyamarych <live-learn@mail.ru>
2019-11-18 20:07:33 -0600

    edited1

--- src/TeamActivityMondayofProject4/Marry.java
+++ src/TeamActivityMondayofProject4/Marry.java
@@ -1,12 +1,22 @@
 package TeamActivityMondayofProject4;
 
 public class Marry {
-
-    public static void main(String[] args) {
-
-        System.out.println("Let's start working");
+    public static boolean isAnagram(String word1, String word2) {
+        word1 = word1.toLowerCase().trim();
+        word2 = word2.toLowerCase().trim();
+        int count = 0;
+        if (word1.length() == word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                if (word2.contains(word1.charAt(i) + "")) {
+                    count++;
+                } else {
+                    return false;
+                }
+            }
+            if (count == word1.length()) {
+                return true;
+            }
+        }
+        return false;
     }
-
-
-
 }

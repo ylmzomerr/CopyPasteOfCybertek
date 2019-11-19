d9ffd5f1c5a58f2cb2fc93fc5889294bfd329893
Tahaalhammadi2020 <taehemarati@gmail.com>
2019-11-18 18:57:20 -0600

    Everthiing workss

--- src/TeamActivityMondayofProject4/Taha.java
+++ src/TeamActivityMondayofProject4/Taha.java
@@ -3,5 +3,10 @@ package TeamActivityMondayofProject4;
 import java.util.Scanner;
 
 public class Taha {
+    public static void main(String[] args) {
+        String word = "    United Arab Emirate    ";
+        System.out.println(word.trim());
+
+    }
 
 }
